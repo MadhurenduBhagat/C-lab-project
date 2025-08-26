831266f0670567a341552c164d9844e81e71e483
Madhurendu Bhagat <madhurendu.bhagat11@gmail.com>
2025-08-26 23:55:05 +0530

    Add files via upload

---
+++ Experiment 1/print address in multiple line.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Bhadra Resisdeny\nFlat-2A,Block-A\npaltan Bazar\nGHY-781007");
+    return 0;
+}
---
+++ Experiment 1/sum of two number pre defined values.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num1=4,num2=5,sum;
+    sum=num1+num2;
+    printf("the sum of %d and %d is %d\n",num1,num2,sum);
+    return 0;
+}
